64d7b6c23300a8a53efd498a174c26c4be7c17a1
Siriyakorn Mekchay <konnoyuuki200@gmail.com>
2024-12-04 11:22:19 +0700

    First Commit

---
+++ Lapsheet2/LabSheet02/src/Lab202.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Lab202 {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int number;	
+		
+		while(true) {
+			System.out.print("Input number : ");
+			number = input.nextInt();
+			
+			if(number<number) {
+				break;
+				System.out.print("BYE BYE");
+			}
+		
+		}
+	}
+}
