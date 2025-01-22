aeb6bc3ec027d8575d06e8a92e5fe2b48c935e6b
Siriyakorn Mekchay <konnoyuuki200@gmail.com>
2025-01-22 08:37:25 +0700

    Attendance

--- LabSheet06/src/LoanCalculator.java
+++ LabSheet06/src/LoanCalculator.java
@@ -1,30 +1,33 @@
 
-public class LoanCalculator {
-	private String productName;
-	private double price;
-	private double interestRate;
-	private int years;
-	
-	public void setLoanDetails(String name,double prodPrice,double intRate,int debtYears) {
-		productName = name;
-		price = prodPrice;
-		interestRate = intRate/100/12;
-		years = debtYears;
-	}
-	
-	public double calculateMonthlyPayment() {
-		double monthlyInterestRate = interestRate;
-		int numOfPayments = years *12;
-		double monthlyPayment = price*monthlyInterestRate*Math.pow(1+monthlyInterestRate, numOfPayments)/(Math.pow(monthlyInterestRate, numOfPayments)-1);
-		return monthlyPayment;
-	}
-	
-	public void displayLoanDetails() {
-		System.out.println("Product : "+ productName);
-		System.out.println("Price : "+ price);
-		System.out.println("Interest Rate: "+ interestRate + "%");
-		System.out.println("Loan Period: "+ years + "years");
-		System.out.println("Monthly Payment: "+ calculateMonthlyPayment());
-	}
+public class LoanCalculator 
+{
+    private String name;
+    private double price;
+    private double interestRate;
+    private double years;
 
-}
+    public void setLoanDetails(String productName, double dbprice, double dbinterestRate, int dbyears) 
+    {
+        name = productName;
+        price = dbprice;
+        interestRate = dbinterestRate;
+        years = dbyears;
+    }
+
+    public double calculateMonthlyPayment() 
+    {
+        double month = years * 12;
+        double interestRatee = (interestRate / 12) / 100;
+        return (price * interestRatee * (Math.pow((1 + interestRatee), month))) /
+               ((Math.pow((1 + interestRatee), month)) - 1);
+    }
+
+    public void displayLoanDetails() 
+    {
+        System.out.println("Product: " + name);
+        System.out.println("Price: " + price);
+        System.out.println("Interest Rate: " + interestRate + "%");
+        System.out.println("Loan Period: " + years + " years");
+        System.out.println("Monthly Payment: " + calculateMonthlyPayment());
+    }
+}    
