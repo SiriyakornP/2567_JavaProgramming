2f93aafecbad3f9bb5f33170ccf76c039cc178e4
Siriyakorn Mekchay <konnoyuuki200@gmail.com>
2025-02-19 11:11:30 +0700

    LabSheet10_Practice

---
+++ LabSheet10/src/Coffee.java
@@ -0,0 +1,16 @@
+
+public class Coffee {
+	public int type;
+	public int typeCoffee;
+	public char sizeCoffee;
+	public Barista barista;
+	
+	Coffee(int type,char sizeCoffee,int typeCoffee,Barista barista){
+		this.type = type;
+		this.sizeCoffee = sizeCoffee;
+		this.typeCoffee = typeCoffee;
+		this.barista = barista;
+	}
+	
+	Coffee(int type, )
+}
